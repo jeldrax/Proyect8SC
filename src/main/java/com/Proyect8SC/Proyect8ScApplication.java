87524238de7d5a0c319be5ada8e6d26ff95ca5d0
pablo <pabloe080404@mal.com>
2025-08-31 19:22:22 -0600

    init commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/Proyect8SC/Proyect8ScApplication.java
@@ -0,0 +1,13 @@
+package com.Proyect8SC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyect8ScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyect8ScApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyect8SC/Proyect8ScApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyect8SC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyect8ScApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
